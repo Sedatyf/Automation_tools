090ac82be4fab24c892edc1e70e24bf6e1971cea
Sedatyf <gaxel@hotmail.fr>
2020-10-09 11:38:56 +0200

    Create setup.py

---
+++ ba_helper/setup.py
@@ -0,0 +1,13 @@
+"""Fichier d'installation de mon script ba_helper.py."""
+
+from cx_Freeze import setup, Executable
+
+# On appelle la fonction setup
+setup(
+    name = "ba_helper",
+    version = "1.0.1",
+    description = "Ce programme aide à l'autocomplétion avec le plugin Cucumber pour VSCode",
+    executables = [Executable("ba_helper.py")],
+)
+
+# Pour la suite, exécuter la commande suivante : python.exe setup.py build
